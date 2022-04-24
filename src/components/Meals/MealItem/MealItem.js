e4dc9c7145194d05b37e37cc8bb537f586cee992
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-04-24 18:34:33 -0300

    Address Form, add Input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -11,6 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
