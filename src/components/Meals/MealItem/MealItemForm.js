ae4b84920787951a8a7e6197d45ce8b96c638586
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-04-25 16:38:45 -0300

    Add CartContext & Cart Reducer, mange state with Refs a& Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,37 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  // Input is a custome component so to make refs work I had to  
+  // wrap the component function (where I want to receive refs) with React.forwardRef
+  // So that component function is now an argument to forwardRef
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; // it returns a string number
+    const enteredAmountNumber = +enteredAmount; // this convert string number into a number
+
+    if (
+      enteredAmount.trim().length === 0 || 
+      enteredAmountNumber < 1 || 
+      enteredAmountNumber > 5
+      ) {
+        setAmountIsValid(false);
+        return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
@@ -16,6 +43,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
