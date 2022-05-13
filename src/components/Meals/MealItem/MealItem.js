673cf17af9025d632f1382305c430f2448cbe4e9
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-05-13 14:34:02 -0300

    Fetch Meals via http

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,21 +1,21 @@
 import { useContext } from 'react';
 
 import MealItemForm from './MealItemForm';
-import CartContext from '../../../store/cart-context';
 import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = (amount) => {
+  const addToCartHandler = amount => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
       price: props.price
-    })
+    });
   };
 
   return (
